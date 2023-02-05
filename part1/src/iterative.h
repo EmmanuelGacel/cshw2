e2c4a5845bf367f70b9f190a356f590976b58bd5
katiewimmer <krwim78@gmail.com>
2023-02-05 13:00:49 -0500

    added header guards

--- part1/src/iterative.h
+++ part1/src/iterative.h
@@ -1,3 +1,6 @@
-//iterative.h
+#ifndef _ITERATIVE_H_
+#define _ITERATIVE__H_
 
 int gcd_iterative(int m, int n);
+
+#endif
--- part1/src/recursive.h
+++ part1/src/recursive.h
@@ -1,3 +1,6 @@
-//recursive.h
+#ifndef _RECURSIVE_H_
+#define _RECURSIVE_H_
 
 int gcd_recursive(int m, int n);
+
+#endif
