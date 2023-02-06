9a6677c946b891afc0ef8e19942bef2585276c0c
Emmanuel Gacel <evg2110@columbia.edu>
2023-02-06 12:06:59 -0500

    binary operation tests

--- part2/src/convert.c
+++ part2/src/convert.c
@@ -4,17 +4,17 @@
 int main() {
 	int x;
 	unsigned int y;
-	int MSB;
+	//int MSB;
 
         printf("Enter an integer   : ");
         scanf("%d", &x);
 	printf("\n");
 	y=x;
-	MSB=(x & (0x1 << ((sizeof(x) << 3) - 1)));
+	printf("%d\n",(x & (0x1 << ((sizeof(x) << 3) - 1))));
 	printf("signed decimal     : %d\n", x); 
 	printf("unsigned decimal   : %u\n", y);
 	printf("hexadecimal number : %x\n", y);
-	printf("%d\n", MSB);
+//printf("%d\n", MSB);
 	
 
     return EXIT_SUCCESS;
