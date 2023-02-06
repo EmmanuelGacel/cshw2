b316b8ac1960064471e10b386d569f0f78fa9520
Emmanuel Gacel <evg2110@columbia.edu>
2023-02-06 09:28:42 -0500

    Tested bitwise operations

--- part2/src/convert.c
+++ part2/src/convert.c
@@ -3,13 +3,19 @@
 
 int main() {
 	int x;
+	unsigned int y;
+	int MSB;
+
         printf("Enter an integer   : ");
         scanf("%d", &x);
 	printf("\n");
-	printf("signed decimal     : %d\n", x); 
-	unsigned int y;
 	y=x;
+	MSB=(x & (0x1 << ((sizeof(x) << 3) - 1)));
+	printf("signed decimal     : %d\n", x); 
 	printf("unsigned decimal   : %u\n", y);
 	printf("hexadecimal number : %x\n", y);
+	printf("%d\n", MSB);
+	
+
     return EXIT_SUCCESS;
 }
